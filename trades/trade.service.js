d5aef66337a81e88c3855a68c8d55efe97577729
Nuri Engin <nuri@engin.web.tr>
2021-12-02 14:43:36 +0300

    📝 : Source code coments improvments

--- trades/trade.service.js
+++ trades/trade.service.js
@@ -1,3 +1,6 @@
+// Call the constants/configs
+const { STATUS_CODES } = require('../constants');
+
 // Call the utils/helper functions
 const generateMarketPair = require('../utils/generateMarketPair');
 const generateFTXOrderBookURL = require('../utils/generateFTXOrderBookURL');
@@ -10,6 +13,9 @@ module.exports = {
 };
 
 /**
+ * Main handler function to grab consumer request payload, compare it with FTX `orderbook` API,
+ * Provide calculations to find the best offer and response back it to consumer.
+ * 
  * @param action (String): Either “buy” or “sell”
  * @param base_currency (String): The currency to be bought or sold
  * @param quote_currency (String): The currency to quote the price in
@@ -22,34 +28,42 @@ module.exports = {
  */
  async function proceedTradeRequest({ res, action, base_currency, quote_currency, amount }) {
      // Ensure to convert amount to the Number!
-     // Let convert the 'amount' if exist...
      amount = Number(amount);
      
-     // 1. Determine between 'bids/asks': Consumer's "BUY" request goes to orderbook.asks and "SELL" request goes into the orderbook.bids.
+     // Determine between 'bids/asks': Consumer's "BUY" request goes to orderbook.asks and "SELL" request goes into the orderbook.bids.
+     const bidsOrAsks = determineBidsOrAsks(action);
      const marketPair = generateMarketPair(base_currency, quote_currency);
      const orderBookURL = generateFTXOrderBookURL(marketPair); 
 
-     // Get all ORDERS from market
+     // Get all ORDERS from market.
      const orderBooks = await getOrderBookResponse(orderBookURL, res);
 
-     // Find out consumer asks for BIDS or ASKS
-     const bidsOrAsks = determineBidsOrAsks(action);
-     const filteredOrderBooks = !!orderBooks && !!bidsOrAsks && orderBooks.message[bidsOrAsks]
+     // Find out consumer based orderbooks stack.
+     const filteredOrderBooks = !!orderBooks && !!bidsOrAsks && orderBooks.message[bidsOrAsks];
 
-     // 2. Grab the consumer's 'amount': We will need to use 'amount' value provided by the consumer in both case for 'bids/asks'.
-     // 3. Compare to orderbook: Find the most closest 'amount' value from orderbook regarding 'bids/asks' differentiation.
+     // Grab the consumer's 'amount': We will need to use 'amount' value provided by the consumer in both case for 'bids/asks'.
+     // Compare to orderbook: Find the most closest 'amount' value from orderbook regarding 'bids/asks' differentiation.
      const comparedOrder = !!filteredOrderBooks && findClosest(filteredOrderBooks, amount);
 
-     // 4. Left-out amount: So we have compared the values and now store the extra value between two amount of orderbook and consumer.
-     const totalAmount = amount + comparedOrder.gap;
+     // Left-out amount: So we have compared the values and now store the extra value between two amount of orderbook and consumer.
+     const totalAmount = !!comparedOrder && amount + comparedOrder.gap;
 
-     // 5. Aggreation process: Multiply one unit amount price to the left-out amount value (left-out amount * one-unit amount price)
-     const aggreatedPrice = totalAmount * comparedOrder.unitPrice ; 
+     // Aggreation process: Multiply one unit amount price to the left-out amount value (left-out amount * one-unit amount price)
+     const aggreatedPrice = !!totalAmount && totalAmount * comparedOrder.unitPrice; 
 
-     // 6.Response back to the consumer with the final quote which is a weighted average of newly price.
+     // Response back to the consumer; 
+     // Firstly ensure no any problem left behind!
+     if (!aggreatedPrice && !totalAmount) {
+        return {
+            status: STATUS_CODES.badRequest,
+            message: "Something went wrong!"
+        };
+     }
+
+     // If all fine, then, let's inform the consumer...
      return {
-         total: totalAmount.toFixed(4), //Total quantity of quote currency
-         price: aggreatedPrice.toFixed(4), // The per-unit cost of the base currency
-         currency: quote_currency
+        total: totalAmount.toFixed(4), 
+        price: aggreatedPrice.toFixed(4),
+        currency: quote_currency
      };
 }
